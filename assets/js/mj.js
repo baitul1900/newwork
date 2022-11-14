0cc042daa5907995268e2c80f5b1c7de853002d0
baitul1900 <baitulamin1900@gmail.com>
2022-11-14 18:05:57 +0600

    gil

--- assets/js/mj.js
+++ assets/js/mj.js
@@ -1,13 +1,14 @@
 $('.responsive').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll: 4,
   responsive: [
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 2,
+        slidesToShow: 3,
         slidesToScroll: 3,
         infinite: true,
         dots: true
@@ -32,3 +33,4 @@ $('.responsive').slick({
     // instead of a settings object
   ]
 });
+				
